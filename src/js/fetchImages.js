f8bccc91df7b79d2c6367447c284021a7ec20551
Maksym Kotyk <mxm.kotyk@gmail.com>
2023-04-01 14:57:27 +0200

    Added pagination

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,17 +1,20 @@
 export class PixabayAPI {
   #BASE_URL = 'https://pixabay.com/';
   #API = '34914076-a1f4109a1a2ff90f5349ae488';
+  #searchQuery = '';
+  #page = 1;
   #searchParams = new URLSearchParams({
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    per_page: 40,
   });
 
-  getImages(searchQuery) {
+  getImages() {
     return fetch(
-      `${this.#BASE_URL}api/?key=${this.#API}&q=${searchQuery}&${
+      `${this.#BASE_URL}api/?key=${this.#API}&q=${this.#searchQuery}&${
         this.#searchParams
-      }`
+      }&page=${this.#page}`
     ).then(response => {
       console.log('response:', response);
 
@@ -21,5 +24,17 @@ export class PixabayAPI {
 
       return response.json();
     });
+  }
+
+  set query(newSearchQuery) {
+    this.#searchQuery = newSearchQuery;
+  }
+
+  incrementPage() {
+    this.#page += 1;
+  }
+
+  resetPage() {
+    this.#page = 1;
   }
 }
--- src/js/searchImages.js
+++ src/js/searchImages.js
@@ -1,4 +1,5 @@
 import { PixabayAPI } from './fetchImages';
+import { Notify } from 'notiflix';
 import { createGalleryMarkup } from './createMarkup';
 
 const searchService = new PixabayAPI();
@@ -6,21 +7,52 @@ export const refs = {
   searchForm: document.querySelector('.js-search-form'),
   searchBox: document.querySelector('.js-search-box'),
   searchBtn: document.querySelector('.js-search-button'),
+  loadMoreBtn: document.querySelector('.load-more'),
   gallery: document.querySelector('.gallery'),
 };
 
 refs.searchForm.addEventListener('submit', hendleSearchOnSubmit);
+refs.loadMoreBtn.addEventListener('click', loadMoreOnClick);
 
 function hendleSearchOnSubmit(e) {
   e.preventDefault();
-  const searchQuery = refs.searchBox.value;
+  clearGalleryMarkup();
 
+  searchService.resetPage();
+  searchService.query = refs.searchBox.value;
+
+  fetchImages();
+}
+
+function loadMoreOnClick() {
+  searchService.incrementPage();
+  fetchImages();
+}
+
+function fetchImages() {
   searchService
-    .getImages(searchQuery)
+    .getImages()
     .then(data => {
       console.log(data);
-      const markup = createGalleryMarkup(data.hits);
-      refs.gallery.insertAdjacentHTML('beforeend', markup);
+
+      if (data.hits.length === 0) {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+      insertGalleryMarkup(data);
+      refs.loadMoreBtn.classList.remove('visually-hidden');
     })
     .catch(error => console.warn(error));
+}
+
+function insertGalleryMarkup(data) {
+  const markup = createGalleryMarkup(data.hits);
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearGalleryMarkup() {
+  refs.gallery.innerHTML = '';
+  refs.loadMoreBtn.classList.add('visually-hidden');
 }
